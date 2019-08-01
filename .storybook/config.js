657e03dd5c7225f5814b2706b344998e63f175c9
Key Houck <key.houck@griffingroupglobal.com>
2019-08-01 12:42:08 -0400

    Master: Loading and Updates

---
+++ .storybook/config.js
@@ -0,0 +1,13 @@
+import { configure } from '@storybook/react';
+// babel macro to ensure require.context runs in Jest
+import requireContext from 'require-context.macro';
+
+import '../src/index.css';
+
+const req = requireContext('../src', true, /.stories.js$/);
+
+function loadStories() {
+  req.keys().forEach(filename => req(filename));
+}
+
+configure(loadStories, module);
